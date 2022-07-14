ce1771b5fe563efcfc3ef7eafa5e935be02b4257
lackerpack <thapaanil2007@gmail.com>
2022-07-14 18:33:44 -0500

    create function computerplay

---
+++ script.js
@@ -0,0 +1,15 @@
+function computerPlay(){
+    roll = Math.floor(Math.random()*3)
+    if (roll === 1){
+        return "Rock"
+    }
+    else if (roll === 2){
+        return "Paper"
+    }
+    else if (roll === 3){
+        return "Scissors"
+    }
+    else{
+        console.log("Error in the code.")
+    }
+}
