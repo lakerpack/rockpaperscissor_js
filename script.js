dc080e087effaaa6723fad755ab8daec924ade94
lackerpack <thapaanil2007@gmail.com>
2022-07-18 17:01:19 -0500

    Fix score system

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ function computerPlay(){
 function checkWinner(player, computer){
     switch (player){
         case "Rock":
-            return computer === "Rock" ? "Tie" : computer === "Paper" ? "Lose" : "Win";
+            return (computer === "Rock") ? "Tie" : (computer === "Paper") ? "Lose" : "Win";
         case "Paper":
-            return computer === "Rock" ? "Win" : computer === "Paper" ? "Tie" : "Lose";
+            return (computer === "Rock") ? "Win" : (computer === "Paper") ? "Tie" : "Lose";
         case "Scissors":
-            return computer === "Rock" ? "Lose" : computer === "Paper" ? "Win" : "Tie";
+            return (computer === "Rock") ? "Lose" : (computer === "Paper") ? "Win" : "Tie";
     }
 }
 
@@ -29,27 +29,56 @@ function checkWinner(player, computer){
 function playRound(choice){
     let computer = computerPlay();
     let result = checkWinner(choice, computer);
+    console.log(result);
     switch (result){
         case "Win":
             console.log(`You win! ${choice} beats ${computer}`);
             updateGame("Win");
+            break;
         case "Tie":
             console.log(`You tied! ${choice} ties with ${computer}`);
             updateGame("Tie");
+            break;
         case "Lose":
             console.log(`You lose! ${choice} loses to ${computer}`);
             updateGame("Lose");
+            break;
     }
 }
 
-function updateGame(result){
+function updateGame(result){    
     switch(result){
         case "Win":
             wins += 1;
+            break;
         case "Lose":
             losses += 1;
+            break;
         case "Tie":
             ties += 1;
+            break;
+    }
+    score.textContent = `Wins: ${wins} Losses: ${losses} Ties: ${ties}`;
+    if (wins == 5 || losses == 5){
+        const restart = document.createElement("button");
+        restart.textContent = "Restart the game?";
+        score.appendChild(restart);
+        if (wins == 5){
+            const endmsg = document.createElement("h4");
+            endmsg.textContent = "You won! Congratulations!";
+            score.appendChild(endmsg);
+        }
+        else{
+            const endmsg = document.createElement("h4");
+            endmsg.textContent = "You lost! Better luck next time!";
+            score.appendChild(endmsg);
+        }
+        restart.addEventListener('click', () => {
+            wins = 0;
+            losses = 0;
+            ties = 0;
+            score.textContent = `Wins: ${wins} Losses: ${losses} Ties: ${ties}`;
+        });
     }
 }
 
@@ -62,7 +91,6 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         const id = button.getAttribute('id')
         playRound(id);
-        score.textContent = `Wins: ${wins} Losses: ${losses} Ties: ${ties}`;
     });
 });
 
@@ -72,16 +100,3 @@ let losses = 0;
 let ties = 0;
 
 const score = document.querySelector("#result");
-
-
-if (wins == 5 || losses == 5){
-    const restart = document.createElement("button");
-    restart.textContent = "Restart the game?";
-    score.appendChild(restart);
-    restart.addEventListener('click', () => {
-        wins = 0;
-        losses = 0;
-        ties = 0;
-    });
-}
-
